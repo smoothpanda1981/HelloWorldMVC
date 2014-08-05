72b2a035048a68ed6f0a05239093797f1a843758
Yan Wang <ywang@acdsystems.com>
2014-08-05 22:12:33 +0200

    3rd commit.

---
+++ src/main/java/com/yan/wang/mvc/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.yan.wang.mvc.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String main(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+//		model.addAttribute("msg", "Yan Wang");
+		
+		return "main";
+	}
+	
+}
---
+++ src/main/java/com/yan/wang/mvc/view/JstlView.java
@@ -0,0 +1,7 @@
+package com.yan.wang.mvc.view;
+
+import org.springframework.web.servlet.view.InternalResourceView;
+
+public class JstlView extends InternalResourceView {
+
+}
